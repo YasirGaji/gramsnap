560cfc441f1385833ec22bd6298168cbf6211f53
Yasir Gaji <gajiyasir@gmail.com>
2023-11-02 09:15:28 +0100

    re-configured the globalscss file and the tailwind config file

--- src/_auth/forms/SignupForms.tsx
+++ src/_auth/forms/SignupForms.tsx
@@ -1,12 +1,72 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { SignupValidation } from "@/lib/validation"
+import { z } from "zod"
 
+
+
+
+// const formSchema = z.object({
+//   username: z.string().min(2).max(50),
+// })
 
 
 const SignupForms = () => {
+  //Form definition 
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  })
+
+  //Submit handler
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
+    console.log(values)
+  }
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Form {...form} >
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="logo" />
+
+        <h2 className="">Create a new account</h2>
+      </div>
+
+
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                Form
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
