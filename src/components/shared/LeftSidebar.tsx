fcfb8b1573082f73f0bee90170098f00435c87ba
Yasir Gaji <gajiyasir@gmail.com>
2023-11-12 18:12:08 +0100

    taking a break

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
+    <nav className='leftsidebar'>
+      <div className='flex flex-col gap-11'>
+        <Link to="/" className='flex gap-3 items-center'>
+          <img 
+            src='/assets/images/logo2.svg'
+            alt='logo'
+            width={170}
+            height={36}
+          />
+        </Link>
+
+        <Link to={'/profile/${user.id}'}></Link>
+      </div>
+    </nav>
   )
 }
 
