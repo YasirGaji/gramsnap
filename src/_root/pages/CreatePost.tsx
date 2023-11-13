de6ca9064201bb75a16c2839dcd9fbef4223d93c
Yasir Gaji <gajiyasir@gmail.com>
2023-11-14 00:31:59 +0100

    done with the create post form

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,8 +1,23 @@
+import PostForm from '@/components/forms/PostForm'
 import React from 'react'
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className='flex flex-1'>
+      <div className='common-container'>
+        <div className='max-w-full flex-start gap-3 justify-start w-full'>
+          <img
+            src="/assets/icons/add-post.svg"
+            alt='add-post'
+            width={36}
+            height={36}
+          />
+          <h2 className='h3-bold md:h2-bold text-left w-full'>Create Post</h2>
+        </div>
+
+        <PostForm />
+      </div>
+    </div>
   )
 }
 
