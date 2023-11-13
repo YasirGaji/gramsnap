af10e7fc5de556c2a9960d179a36eb5abf58b67c
Yasir Gaji <gajiyasir@gmail.com>
2023-11-13 23:04:21 +0100

    worked on and fixed the routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Routes, Route } from 'react-router-dom';
 import SigninForms from './_auth/forms/SigninForms';
 import SignupForms from './_auth/forms/SignupForms';
-import { Home } from './_root/pages';
+import { AllUsers, CreatePost, EditPost, Explore, Home, PostDetails, Profile, Saved, UpdateProfile } from './_root/pages';
 import AuthLayout from './_auth/AuthLayout';
 import RootLayout from './_root/RootLayout';
 import { Toaster } from '@/components/ui/toaster'
@@ -25,6 +25,14 @@ const App = () => {
         {/*Private Routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} />
         </Route>
       </Routes>
 
