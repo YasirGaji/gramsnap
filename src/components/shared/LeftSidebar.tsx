cc3c83a61f3775f85ee6eee4a4173a4f122d4466
Yasir Gaji <gajiyasir@gmail.com>
2023-11-13 18:12:23 +0100

    resumed

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { Button } from '../ui/button'
+import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations'
+import { useEffect } from 'react';
+import { useUserContext } from '@/context/AuthContext';
 
 const LeftSidebar = () => {
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const { user } = useUserContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess])
+
   return (
     <nav className='leftsidebar'>
       <div className='flex flex-col gap-11'>
@@ -14,7 +25,12 @@ const LeftSidebar = () => {
           />
         </Link>
 
-        <Link to={'/profile/${user.id}'}></Link>
+        <Link to={'/profile/${user.id}'} className='flex gap-3 items-center'>
+          <img 
+            src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+            alt='profile'
+          />
+        </Link>
       </div>
     </nav>
   )
