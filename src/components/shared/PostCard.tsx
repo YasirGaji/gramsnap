be4d5dbb60f5541b95cb2ec22155962eaa833105
Yasir Gaji <gajiyasir@gmail.com>
2023-11-17 21:14:44 +0100

    rendered post stats static ui

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -2,6 +2,7 @@ import { useUserContext } from "@/context/AuthContext";
 import { formatDateString } from "@/lib/utils";
 import { Models } from "appwrite";
 import { Link } from "react-router-dom";
+import PostStats from "./PostStats";
 
 type PostCardProps ={
   post: Models.Document;
@@ -73,6 +74,8 @@ const PostCard = ({ post }: PostCardProps)=> {
           alt="post-image"
         />
       </Link>
+
+      <PostStats post={post} userId={user.id} />
     </div>
   )
 }
---
+++ src/components/shared/PostStats.tsx
@@ -0,0 +1,41 @@
+import { Models } from "appwrite"
+
+
+
+
+type PostStatsProps = {
+  post: Models.Document;
+  userId: string;
+}
+
+const PostStats = ({ post, userId}: PostStatsProps) => {
+  return (
+    <div className="flex justify-between items-center z-20 mt-5">
+      <div className="flex gap-2 mr-5">
+        <img 
+          src="/assets/icons/like.svg"
+          alt="like icon"
+          width={20}
+          height={20}
+          onClick={() => {}}
+          className="cursor-pointer"
+        />
+        <p className="small-medium lg:base-medium">0</p>
+      </div>
+
+      <div className="flex gap-2">
+        <img 
+          src="/assets/icons/save.svg"
+          alt="like icon"
+          width={20}
+          height={20}
+          onClick={() => {}}
+          className="cursor-pointer"
+        />
+        <p className="small-medium lg:base-medium">0</p>
+      </div>
+    </div>
+  )
+}
+
+export default PostStats
