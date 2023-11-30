b6db99c705c39caa3c888038e3dc5c5da150bd30
Yasir Gaji <gajiyasir@gmail.com>
2023-11-30 22:31:43 +0100

    search bar UI complete

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+
 
 const Explore = () => {
+  const [searchValue, setSearchValue] = useState('')
+
   return (
-    <div>Explore</div>
+    <div className="explore-container">
+      <div className="explore-inner_container">
+        <h2 className="h3-bold">Search Posts</h2>
+
+        <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
+          <img 
+            src="/assets/icons/search.svg"
+            width={24}
+            height={24}
+            alt="search bar"
+          />
+
+          <Input
+            type="text"
+            placeholder="search"
+            className="explore-search"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
