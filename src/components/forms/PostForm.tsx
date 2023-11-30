66b5a1a39a0282a4e66e84d2738f05983ec70085
Yasir Gaji <gajiyasir@gmail.com>
2023-11-30 17:19:04 +0100

    was able to update a post succesfully

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -148,6 +148,7 @@ const PostForm = ({ post, action }: PostFormProps) => {
 
           <div className="flex gap-4 items-center justify-end">
             <Button type="button" className="shad-button_dark_4">Cancel</Button>
+            
             <Button type="submit" className="shad-button_primary whitespace-nowrap"
             disabled={ isLoadingCreate || isLoadingUpdate }
             >
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -276,34 +276,31 @@ export async function getPostById(postId?: string) {
 export async function updatePost(post: IUpdatePost) {
   const hasFileToUpdate = post.file.length > 0;
 
-
   try {
     let image = {
       imageUrl: post.imageUrl,
       imageId: post.imageId,
-    }
+    };
 
-    if(hasFileToUpdate) { 
-      // Upload file to appwrite storage
+    if (hasFileToUpdate) {
+      // Upload new file to appwrite storage
       const uploadedFile = await uploadFile(post.file[0]);
-
       if (!uploadedFile) throw Error;
 
-      // Get file url
+      // Get new file url
       const fileUrl = getFilePreview(uploadedFile.$id);
       if (!fileUrl) {
         await deleteFile(uploadedFile.$id);
         throw Error;
       }
 
-      image = { ...image, imageUrl: fileUrl, imageId: uploadedFile.$id }
+      image = { ...image, imageUrl: fileUrl, imageId: uploadedFile.$id };
     }
-
 
     // Convert tags into array
     const tags = post.tags?.replace(/ /g, "").split(",") || [];
 
-    // Create post
+    //  Update post
     const updatedPost = await databases.updateDocument(
       appwriteConfig.databaseId,
       appwriteConfig.postCollectionId,
@@ -312,14 +309,26 @@ export async function updatePost(post: IUpdatePost) {
         caption: post.caption,
         imageUrl: image.imageUrl,
         imageId: image.imageId,
+        postId: post.postId,
         location: post.location,
         tags: tags,
       }
     );
 
+    // Failed to update
     if (!updatedPost) {
-      await deleteFile(post.imageId);
+      // Delete new file that has been recently uploaded
+      if (hasFileToUpdate) {
+        await deleteFile(image.imageId);
+      }
+
+      // If no new file uploaded, just throw error
       throw Error;
+    }
+
+    // Safely delete old file after successful update
+    if (hasFileToUpdate) {
+      await deleteFile(post.imageId);
     }
 
     return updatedPost;
@@ -328,21 +337,27 @@ export async function updatePost(post: IUpdatePost) {
   }
 }
 
-export async function deletePost(postId: string, imageId: string) {
-  if(!postId || !imageId) throw Error;
+// ============================== DELETE POST
+export async function deletePost(postId?: string, imageId?: string) {
+  if (!postId || !imageId) return;
 
   try {
-    await databases.deleteDocument(
+    const statusCode = await databases.deleteDocument(
       appwriteConfig.databaseId,
       appwriteConfig.postCollectionId,
       postId
-    )
+    );
 
-    return { status: 'ok' }
+    if (!statusCode) throw Error;
+
+    await deleteFile(imageId);
+
+    return { status: "Ok" };
   } catch (error) {
     console.log(error);
   }
 }
+
 
 
 
