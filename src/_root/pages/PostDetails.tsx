ebcc1371e182e6da7e125d0ad7b5c065443b62a6
Yasir Gaji <gajiyasir@gmail.com>
2023-11-30 18:25:36 +0100

    rendered the post details from the database to the UI

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations"
+import { formatDateString } from "@/lib/utils";
+import { Loader } from "lucide-react";
+import { Link, useParams } from "react-router-dom";
 
 const PostDetails = () => {
+  const { id } = useParams();
+  const { data: post, isPending } = useGetPostById(id);
+
+
   return (
-    <div>PostDetails</div>
+    <div className="post_details-container">
+      {isPending ? <Loader /> : (
+        <div className="post_details-card">
+          
+          <div className="flex items-center gap-3">
+            <Link to={`/profile/${post?.creator.$id}`}>
+              <img
+                src={post?.creator?.imageUrl || 'assets/icons/profile-placeholder.svg'}
+                alt="user-prifile-image"
+                className="rounded-full w-12 lg:h-12"
+              />
+            </Link>
+
+            <div className="flex flex-col">
+              <p className="base-medium lg:body-bold text-light-1">
+                {post?.creator.name}
+              </p>
+
+              <div className="flex-center gap-2 text-light-3">
+                <p className="subtlle-semibold lg:small-regular">
+                  {formatDateString(post?.$createdAt)}
+                </p>
+
+                -
+
+                <p className="subtle-semibold lg:small-regular">
+                  {post?.location}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
 
