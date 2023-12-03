4ac9f3a45d1c308b5d86e43bb6883811e29efeed
Yasir Gaji <gajiyasir@gmail.com>
2023-12-03 20:15:20 +0100

    new

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -12,7 +12,7 @@ const Explore = () => {
   // const shouldShowSearchResults = searchValue !== '';
   // const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item) => item.documents.length === 0)
 
-  ////for push 
+
 
   return (
     <div className="explore-container">
